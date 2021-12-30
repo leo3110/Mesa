6e2f824396610fafd8cf1bd51a67213db9a08c84
Leo <leo3110.souza@gmail.com>
2021-12-30 18:56:38 -0300

    Start

---
+++ GUI/mesa.php
@@ -0,0 +1,37 @@
+<div class="Mesa">
+
+  <div class="Ficha">
+    <div class="Atributos">
+      <div><i class="fas fa-heartbeat"></i>  Vida: <span id="vidaT">20</span>/<span id="vidaM">20</span></span></div>
+      <div><i class="fas fa-magic"></i>  Mana: <span id="manaT">20</span>/<span id="manaM">20</span>  </span></div>
+      <div><i class="fas fa-heart"></i>  Vigor: <span id="vigor">  </span></div>
+      <div><i class="fas fa-fist-raised"></i>  Força: <span id="forca">  </span></div>
+      <div><i class="fas fa-hand-point-right"></i>  Destreza: <span id="destr">  </span></div>
+      <div><i class="fas fa-wind"></i>  Agilidade: <span id="agili">  </span></div>
+      <div><i class="fas fa-shield-alt"></i>  Resistencia: <span id="resis">  </span></div>
+      <div><i class="fas fa-brain"></i>  Inteligência: <span id="intel">  </span></div>
+      <div><i class="fas fa-scroll"></i>  Conhecimento: <span id="conhe">  </span></div>
+    </div>
+    <div class="Informacao">
+      <span id="nome">Nada - Nada</span>
+      <span id="nivel">1</span>
+      <table>
+
+      </table>
+    </div>
+  </div>
+  <div class="Dados">
+    <div class="">
+      <button id="dado"  type="button" class="" onclick="D20()">D20</button>
+      <button id="dado"  type="button" class="" onclick="D10()">D10</button>
+      <button id="dado"  type="button" class="" onclick="D100()">D100</button>
+      <button id="dado"  type="button" class="" onclick="D6()">D6</button>
+      <button id="dado"  type="button" class="" onclick="Moeda()">Cara ou coroa</button>
+    </div>
+    <div class="Resultado">
+      <span id="resultado">"MESA"</span>
+    </div>
+  </div>
+</body>
+</html>
+</div>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+include 'GUI/header.php';
+include 'GUI/mesa.php';
+include 'GUI/footer.php';
