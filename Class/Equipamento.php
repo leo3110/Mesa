83efa6e0329372525e81c51683d4693d4a3e3e79
Leo <leo3110.souza@gmail.com>
2022-01-14 17:31:50 -0300

    melhorias, finalmente

--- Class/Equipamento.php
+++ Class/Equipamento.php
@@ -7,17 +7,17 @@ class Equipamento
   private $nome = 'espada magica';
   private $acao = ['atacar'=>'for','defender'=>'res','aparar'=>'des','conjurar'=>'int'];
   private $bonus = ['for'=>1,'int'=>1];
-
-  public function info(){
-    echo "<div class='Informacao'>
-    <span id='nome'>Finn - Humano</span>
-    <span id='nivel'>1</span>
-    <div class='💪'>";
-  }
+  //
+  // public function info(){
+  //   echo "<div class='Informacao'>
+  //           <span id='nome'>Finn - Humano</span>
+  //           <span id='nivel'>1</span>
+  //         <div class='Flex'>";
+  // }
   public function equip(){
-    echo "<div class='Equipamento'>";
-    echo "<div class=''>$this->nome</div>";
-    echo "<div class='Botoes 💪 👌'>";
+    echo "<div class='Equipamento Flex Col SAround'>";
+    echo "<div class='EquipamentoNome'>$this->nome</div>";
+    echo "<div class='Botoes Flex Wrap '>";
         foreach ($this->acao as $key => $value) {
           if (isset($this->bonus["$value"])) {
             echo "<button class='Funcao' dado='".$value."' bonus='".$this->bonus["$value"]."'>$key</button>";
--- GUI/mesa.php
+++ GUI/mesa.php
@@ -1,19 +1,22 @@
 <div class="Mesa">
-  <div class="Ficha 💪">
-    <div class="Atributos">
-      <?php
+  <div class="Ficha">
+    <div class="Info Flex SAround">
+      <div class="">
+        <span id="nome">Finn - Humano</span>
+        <span id="nivel">1</span>
+      </div>
+    </div>
+    <div class="Flex">
+      <div class="Atributos Flex Col SAround">
+        <?php
         include './Class/Ficha.php';
         $ficha = new Ficha();
         $ficha->atrib();
-       ?>
-    </div>
-    <div class="Informacao ">
-      <h1>
-        <span id="nome">Finn - Humano</span>
-        <span id="nivel">1</span>
-      </h1>
-      <div class="💪 👌">
-        <?php
+        ?>
+      </div>
+      <div class="Informacao">
+        <div class="EquipList Flex Wrap SAround">
+          <?php
           include './Class/Equipamento.php';
           $equipamento = new Equipamento();
           $equipamento->equip();
@@ -23,14 +26,12 @@
           $equipamento->equip();
           $equipamento->equip();
           $equipamento->equip();
-          $equipamento->equip();
-          $equipamento->equip();
-          $equipamento->equip();
-         ?>
+          ?>
+        </div>
       </div>
     </div>
   </div>
-  <div class="💪 Dados">
+  <div class="Flex Dados">
     <div class="">
       <button id="dado"  type="button" class="" onclick="D20()">D20</button>
       <button id="dado"  type="button" class="" onclick="D10()">D10</button>
